6016a8e3d8c5a274033d30808d3277b70ce6b2fa
Jacqui <jacqui.sedaghat9@gmail.com>
2013-09-06 21:57:28 +1000

    Smoothscroll and fix submenu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
 
+  $("nav a").smoothScroll({speed:1000});
+
+  $(".sub-menu a").smoothScroll({speed:1000});
+
+  $(".top a").smoothScroll({speed:1000});
+
+  $(".service-info a").smoothScroll({speed:1000});
+
+  $.noConflict???
+
+});
